e0029704731fbc6638341008bf95682d5ff11f90
upsidedownloot <upsidedownloot@protonmail.com>
2021-09-08 17:29:50 +0200

    Insert Links

--- pages/index.js
+++ pages/index.js
@@ -67,7 +67,7 @@ export default function Index() {
               <Grid container spacing={6} justifyContent="center">
                 <Grid item>
                   <WhiteTextTypography variant="h6" color="textSecondary" align="center">
-                    <Link color="inherit" href="https://opensea.io/" underline="hover">
+                    <Link color="inherit" href="https://opensea.io/collection/upside-down-loot" underline="hover">
                       OpenSea
                     </Link>
                   </WhiteTextTypography>
@@ -81,7 +81,7 @@ export default function Index() {
                 </Grid>
                 <Grid item>
                   <WhiteTextTypography variant="h6" color="textSecondary" align="center">
-                    <Link color="inherit" href="https://etherscan.io/" underline="hover">
+                    <Link color="inherit" href="https://etherscan.io/token/0x191dfb7e10f4c3a82b2c4f763d48744b6c7cf7ec#writeContract" underline="hover">
                       Contract
                     </Link>{' '}
                   </WhiteTextTypography>
