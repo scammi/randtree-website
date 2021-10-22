9c64253293cf7ed39a35704ed824480e2e3d69c6
Your Name <scammi@gmail.com>
2021-10-22 19:09:21 -0300

    Working provider on click

--- pages/rLOOT.js
+++ pages/rLOOT.js
@@ -6,6 +6,8 @@ import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import Chip from '@material-ui/core/Chip';
 
+import Web3 from 'web3'
+
 import WhiteTextTypography from '../components/WhiteTextTypography';
 
 import Header from '../components/Header';
@@ -46,6 +48,15 @@ const cards = [1, 2];
 export default function Index() {
   const classes = useStyles();
 
+  async function loadBlockChain() {
+    const web3 = new Web3(Web3.givenProvider || 'http://localhost:8080')
+    const network = await web3.eth.net.getNetworkType();
+    console.log(network) // should give you main if you're connected to the main network via metamask...
+    const accounts = await web3.eth.getAccounts()
+
+    console.log({account: accounts[0]})
+  }
+
   return (
     <React.Fragment>
       <CssBaseline />
@@ -63,7 +74,7 @@ export default function Index() {
               </WhiteTextTypography>
             </Grid>
             <Grid item>
-              <Button variant="contained"> Connect </Button>
+              <Button variant="contained" onClick={loadBlockChain}> Connect </Button>
             </Grid>
           </Grid>
 
