e9e5dd4c0d136905b82d047c8a83d98ae7767e07
Your Name <scammi@gmail.com>
2021-10-23 14:50:39 -0300

    Update faq

--- guide/faq.md.js
+++ guide/faq.md.js
@@ -1,5 +1,11 @@
 export default `
 # rLOOT 
+<br>
+<p>An affordable batch base system to mint and distribute NFTs using ChainLink VRF as random entropy</p>
+<br>
+<p>Buy a TICKET to participate in a raffle, you might be ranombly selected to claim a rLOOT!</p>
+<br>
+<p>A loot-box consists of a randomly generated hash, it is constructed using as a source of entropy chain links VRF.</p>
 
 `
 
