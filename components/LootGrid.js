3785ee2f828f4beb7cfbb318000b6ff39aa26eba
Your Name <scammi@gmail.com>
2021-10-25 13:49:34 -0300

    padding for grid

--- components/LootGrid.js
+++ components/LootGrid.js
@@ -73,10 +73,16 @@ export default function LootGrid(props) {
   }, [owner])
 
   return (
-    <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+    <Grid 
+      container
+      spacing={{ xs: 2, md: 3 }}
+      columns={{ xs: 4, sm: 8, md: 12 }}
+      justifyContent="center"
+      style={{margin:"100px"}}
+    >
       {Array.from(loots).map((loot, index) => (
         <Grid item xs={2} sm={4} md={4} key={index}>
-        <WhiteTextTypography>{loot.batchId ?? loot.id}</WhiteTextTypography>
+          <WhiteTextTypography>{loot.batchId ?? loot.id}</WhiteTextTypography>
         </Grid>
       ))}
     </Grid>
