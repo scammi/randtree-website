3a7761a7696583c81682e64fae5876af0036030e
scammi <scammi@gmail.com>
2021-10-30 13:18:15 -0300

    Diver for raffle dinfo

--- components/RaffleCard.js
+++ components/RaffleCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Box, Card, CardActions, CardContent, Typography } from '@material-ui/core';
+import { Button, Box, Card, CardActions, CardContent, Typography, Grid,Divider } from '@material-ui/core';
 import BuyButton from '../components/buttons/BuyButton';
 
 export default function RaffleCard(props) {
@@ -7,19 +7,36 @@ export default function RaffleCard(props) {
   return(
     <Card sx={{ minWidth: 275 }}>
       <CardContent>
-        <Typography color="text.secondary" gutterBottom>
-          Raffle 
-        </Typography>
-        <Typography>
-          Participants
-        </Typography>
-        <Typography>
-          Ends in
-        </Typography>
+        <Grid container justifyContent="flex-start">
+          <Grid item>
+            <Typography variant="h6">
+              Oct 29 2021
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid container justifyContent="space-evenly" style={{padding:"20px"}}>
+          <Grid item xs={3}>
+            <Typography>
+              Participants
+            </Typography>
+            <Typography variant="h4">
+              98
+            </Typography>
+          </Grid>
+          <Divider orientation="vertical" flexItem/>
+          <Grid item>
+            <Typography>
+              Ends in
+            </Typography>
+            <Typography variant="h4">
+             3h 30m 47s
+            </Typography>
+          </Grid>
+        </Grid>
+        <CardActions>
+          <BuyButton contract={props.contract}/>
+        </CardActions>
       </CardContent>
-      <CardActions>
-        <BuyButton contract={props.contract}/>
-      </CardActions>
     </Card>
   );
 }
