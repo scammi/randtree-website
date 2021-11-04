a8f18ddc2359985344c61939e736e7d8dff1eb56
scammi <scammi@gmail.com>
2021-11-04 12:00:43 -0300

    Fetch img as array

--- components/ArtCard.js
+++ components/ArtCard.js
@@ -8,6 +8,7 @@ import ClaimPriceButton from './buttons/ClaimPrice';
 export default function ArtCard() {
 
   const [ state, setState ] = useAppContext();
+  const [bmp, setBMP] = useState([])
 
   const previousBatch = () => {
     setState((state) => ({
@@ -33,6 +34,74 @@ export default function ArtCard() {
     else return false
   }
 
+  async function getBMP() {
+    // new Uint8Array((await (await fetch('https://ipfs.io/ipfs/QmQUq2KxNCnvzPFXnFJYZ276q1JCnNirKtiWmoiiptwmWQ%27)).arrayBuffer()))
+
+    fetch('https://ipfs.io/ipfs/QmQUq2KxNCnvzPFXnFJYZ276q1JCnNirKtiWmoiiptwmWQ')
+    .then(r => {
+      return r.arrayBuffer()
+    })
+    .then(async (imgArray) => {
+      let img = new Uint8Array(imgArray)
+      console.log(img)
+      setBMP(img)
+    })
+    
+  };
+
+  function getRGBA(color) {
+    return [
+      color & 0xff,
+      (color & 0x00ff00) >> 8,
+      (color & 0xff0000) >> 16,
+      0xFF
+    ]
+  }
+  const pixelSize = 16
+  const imageSize = 32
+  
+  function drawAmplifiedImage(ctx, image) {
+    for (let y = 0; y < 32; y++) {
+      for (let x = 0; x < 32; x++) {
+        const rgba = getRGBA(image[y * 32 + x])
+        console.log(`rgba(${rgba[0]},${rgba[1]},${rgba[2]},${rgba[3]})`)
+
+        ctx.fillStyle = `rgba(${rgba[0]},${rgba[1]},${rgba[2]},${rgba[3]})`
+
+        ctx.fillRect(x * pixelSize, y * pixelSize, pixelSize, pixelSize);
+      }
+    }
+  }
+  
+  const Canvas = ({ data }) => {
+    const canvas = React.useRef();
+    React.useEffect(() => {
+      const context = canvas.current.getContext('2d');
+      if (data.length === 0) return
+      if (canvas.current) {
+        drawAmplifiedImage(context, data)
+      }
+    });
+  
+    return (
+      <canvas
+        ref={canvas}
+        width={imageSize * pixelSize}
+        height={imageSize * pixelSize}
+        style={{
+          border: '1px solid #000000'
+        }}
+      >
+      </canvas >
+    )
+  };
+  React.useEffect(() => {
+    getBMP()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[])
+
+
+
   return(
     <Card style={{ width: "400px" }}>
       <CardContent>
@@ -51,12 +120,7 @@ export default function ArtCard() {
             </Button>
           </Grid>
         </Grid>
-        <CardMedia
-        component="img"
-        alt=""
-        height="400"
-        image="https://art.pixilart.com/63ed34defd26a14.png"
-       />
+        <Canvas data={bmp}/>
         <CardActions>
           <ClaimPriceButton/>
         </CardActions>
