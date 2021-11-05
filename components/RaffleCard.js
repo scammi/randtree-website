d90108460019e0ac52ada290ab7b976ba5009e50
scammi <scammi@gmail.com>
2021-11-05 11:41:57 -0300

    Improved adaptive layout

--- components/RaffleCard.js
+++ components/RaffleCard.js
@@ -9,11 +9,12 @@ import {
   Grid,
   Divider,
 } from "@material-ui/core";
-import Stack from '@mui/material/Stack';
+import Stack from "@mui/material/Stack";
 import BuyButton from "../components/buttons/BuyButton";
 import { useAppContext } from "../context/AppContext";
 import Countdown from "react-countdown";
 import ProcessBatchButton from "./buttons/ProcessBatchButton";
+import { GridGoldenratio } from "@mui/icons-material";
 
 export default function RaffleCard(props) {
   let [state] = useAppContext();
@@ -31,74 +32,67 @@ export default function RaffleCard(props) {
     }
   };
 
-  let cardContent;
+  let cardContent
+  let cardActions
+
   if (state.latestBatch == state.batchOnDisplayIndex + 1 || !state.connected) {
     cardContent = (
       <>
-        <Grid
-          container
-          justifyContent="flex-start"
-          style={{ minWidth: "400px" }}
-        >
-          <Grid item>
-            <Typography variant="h6">Oct 29 2021</Typography>
-          </Grid>
+        <Grid item>
+          <Typography>Participants</Typography>
+          <Typography variant="h4">98</Typography>
         </Grid>
-        <Grid
-          container
-          justifyContent="space-evenly"
-          style={{ padding: "20px" }}
-        >
-          <Grid item xs={3}>
-            <Typography>Participants</Typography>
-            <Typography variant="h4">98</Typography>
-          </Grid>
-          <Divider orientation="vertical" flexItem />
-          <Grid item>
-            <Typography>Ends in</Typography>
-            <Typography variant="h4">
-              <Countdown date={Date.now() + 11900000} renderer={renderer} />
-            </Typography>
-          </Grid>
+        <Divider orientation="vertical" flexItem />
+        <Grid item>
+          <Typography>Ends in</Typography>
+          <Typography variant="h4">
+            <Countdown date={Date.now() + 11900000} renderer={renderer} />
+          </Typography>
         </Grid>
-        <CardActions>
-          <BuyButton contract={state.raffleContract} />
-          <ProcessBatchButton />
-        </CardActions>
       </>
     );
+
+    cardActions = (
+      <>
+        <BuyButton contract={state.raffleContract} />
+        <ProcessBatchButton />
+      </>
+    )
   } else {
     cardContent = (
       <>
-        <Grid container justifyContent="flex-start">
-          <Grid item>
-            <Typography variant="h6">Oct 23 2021</Typography>
-          </Grid>
+        <Grid item>
+          <Typography>Winner</Typography>
+          <Typography variant="caption">
+            {state.batches[state.batchOnDisplayIndex]?.winner}
+          </Typography>
         </Grid>
-          <Stack
-            direction={{ xs: "column", sm: "row" }}
-            spacing={{ xs: 1, sm: 5 }}
-            width={{ xs: "auto", sm: "auto" }}
-            justifyContent="center"
-          >
-            <Grid item>
-              <Typography>Winner</Typography>
-              <Typography variant="caption">
-                {state.batches[state.batchOnDisplayIndex]?.winner}
-              </Typography>
-            </Grid>
-            <Divider orientation="vertical" flexItem />
-            <Grid item>
-              <Typography>Participants</Typography>
-              <Typography variant="h5">98</Typography>
-            </Grid>
-          </Stack>
+        <Divider orientation="vertical" flexItem />
+        <Grid item>
+          <Typography>Participants</Typography>
+          <Typography variant="h5">98</Typography>
+        </Grid>
       </>
     );
   }
   return (
     <Card sx={{ minWidth: 275 }}>
-      <CardContent>{cardContent}</CardContent>
+      <CardContent>
+          <Grid item>
+            <Typography variant="h6">Oct 23 2021</Typography>
+          </Grid>
+        <Stack
+          direction={{ xs: "column", sm: "row" }}
+          spacing={{ xs: 1, sm: 5 }}
+          width={{ xs: "auto", sm: "auto" }}
+          justifyContent="space-between"
+          >
+          {cardContent}
+        </Stack>
+      </CardContent>
+      <CardActions>
+        {cardActions}
+      </CardActions>
     </Card>
   );
 }
